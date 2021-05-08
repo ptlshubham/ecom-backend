e8145a9a2a6798516c6c00b5ba7ee63ace52fdc9
shubham <ptlshubham@gmail.com>
2021-05-09 02:34:37 +0530

    NavbarRoute

--- src/routes/user.js
+++ src/routes/user.js
@@ -205,16 +205,16 @@ router.get("/GetProductDetails/:id", (req, res, next) => {
     });
 });
 
-router.get("/GetProductImages/:id", (req, res, next) => {
-    console.log(req.body.userid)
-    db.executeSql("select * from images where productid =" + req.params.id, function(data, err) {
-        if (err) {
-            console.log("Error in store.js", err);
-        } else {
-            return res.json(data);
-        }
-    });
-});
+// router.get("/GetProductImages/:id", (req, res, next) => {
+//     console.log(req.body.userid)
+//     db.executeSql("select mi.id,mi.productid,p.id as ProductId,p.productMainImage from images mi join product p on where productid =" + req.params.id, function(data, err) {
+//         if (err) {
+//             console.log("Error in store.js", err);
+//         } else {
+//             return res.json(data);
+//         }
+//     });
+// });
 
 
 router.post("/SaveMainCategory", (req, res, next) => {
@@ -392,7 +392,17 @@ router.get("/RemoveReviews/:id", (req, res, next) => {
         }
     });
 })
-
+router.post("/GetNavbarRoutedProducts", (req, res, next) => {
+    console.log("herde");
+    console.log(req.body.id);
+    db.executeSql("select * from product where subCategory=" + req.body.id, function(data, err) {
+        if (err) {
+            console.log("Error in store.js", err);
+        } else {
+            return res.json(data);
+        }
+    });
+})
 
 router.post("/UploadProductImage", (req, res, next) => {
     var imgname = generateUUID();
